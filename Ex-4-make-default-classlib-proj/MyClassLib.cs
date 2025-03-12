4945a449e8a98cbedfc8fb67ad27563639df13fb
Andrei Kovtun <andreykpq@gmail.com>
2025-03-12 21:19:16 +0100

    adds ex 4 to create classlib from scratch using default msbuild tools

---
+++ Ex-4-make-default-classlib-proj/MyClassLib.cs
@@ -0,0 +1,10 @@
+namespace SimpleClassLib
+{
+    public class MyClassLib
+    {
+        public string GetHello()
+        {
+            return "Hello from MyClassLib";
+        }
+    }
+}
---
+++ Ex-4-make-default-classlib-proj/Readme.md
@@ -0,0 +1,3 @@
+# Ex to create default class library using msbuild default targets
+
+just run msbuild.exe SimpleCLassLib.csproj
